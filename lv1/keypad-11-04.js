e285777356d5ea5ac2ef7a78f86867cec69219e3
salangdung-i <dladmswl92@naver.com>
2021-11-04 12:52:44 +0900

    reference keypad

--- lv1/keypad-11-04.js
+++ lv1/keypad-11-04.js
@@ -1,18 +1,61 @@
 function solution(numbers, hand) {
     var answer = '';
-    // 1. numbers을 순차적으로 돌린다. 
-    for(let i = 0;i < numbers.length;i++){
-        // 2. R(3,6,9,#) L(1,4,7,*) 케이스를 나눈다. 
-        if(numbers[i] === 1 || numbers[i] === 4 || numbers[i] === 7 || numbers[i] === '*' ) {
+    const keypad = {
+        1: [1,1], 2: [1,2], 3:[1,3],
+        4: [2,1], 5: [2,2], 6:[2,3],
+        7: [3,1], 8: [3,2], 9:[3,3],
+        '*': [4,1], 0: [4,2], '#':[4,3] 
+    };
+
+    let nowL = [4,1]; 
+    let nowR = [4,3];
+
+    numbers.forEach(num => {
+        let numLocation = keypad[num];
+
+        if(numLocation[1] === 1) {
+            // 왼손
+            nowL = numLocation;
             answer += 'L';
-        } else if(numbers[i] == 3 || numbers[i] == 3 || numbers[i] == 3 || numbers[i] ==  ) {
+        } else if (numLocation[1] === 3) {
+            // 오른손
+            nowR = numLocation;
             answer += 'R';
         } else {
-            // 3. 나머지 케이스를 나눈다. 
-            // 여기서 막혔다. 
+            //위치확인해야함
+            let distanceL = getDistance(nowL, numLocation);
+            let distanceR = getDistance(nowR, numLocation);
+            console.log(`${distanceL}, ${distanceR}`)
+            if(distanceL === distanceR) {
+                if(hand === "left") {
+                    nowL = numLocation;
+                    answer += 'L';
+                } else {
+                    nowR = numLocation;
+                    answer += 'R';
+                }
+            } else if (distanceL < distanceR ){
+                nowL = numLocation;
+                answer += 'L';
+            } else {
+                nowR = numLocation;
+                answer += 'R';
+            }
         }
-    }
+        console.log(`${num} ${numLocation}, ${answer}`)
+    });
 
-    console.log(answer);
+    console.log(`비교 : ${answer}`);
     return answer;
 }
+
+
+function getDistance(arr1, arr2) {
+    let result = Math.abs(arr1[0] - arr2[0]) + Math.abs(arr1[1] - arr2[1]); 
+    return result; 
+}
+
+const numbers = [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5];
+const hand = "right";
+solution(numbers, hand);
+console.log("정답 : LRLLLRLLRRL");
