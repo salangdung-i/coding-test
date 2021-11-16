369cebca0f3605bba91231f5239f23b9d0b1425b
salangdung-i <dladmswl92@naver.com>
2021-11-16 12:50:34 +0900

    diffrent method

--- lv1/add-missing-numbers-11-16.js
+++ lv1/add-missing-numbers-11-16.js
@@ -1,20 +1,9 @@
 function solution(numbers) {
-  var answer = -1;
-  const exitNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  let correct = [];
-  let index;
+  let answer = 0;
 
-  for (let i = 0; i < numbers.length; i++) {
-    index = exitNumbers.indexOf(numbers[i]);
-    correct = exitNumbers.splice(index, 1);
+  for (let i = 0; i <= 9; i++) {
+    if (!numbers.includes(i)) answer += i;
   }
-
-
-  console.log(exitNumbers);
-  console.log(typeof (answer));
-
-  exitNumbers.forEach(e => answer += e);
-  console.log(answer);
   return answer;
 }
 
