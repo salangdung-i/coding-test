b2c6d57b0b6bd495af98fac36df56e0b68ab58c2
salangdung-i <dladmswl92@naver.com>
2021-11-21 13:41:29 +0900

    sum dot products sovle

--- lv1/sum-dot-products-11-12.js
+++ lv1/sum-dot-products-11-12.js
@@ -1,4 +1,16 @@
 function solution(a, b) {
-  var answer = 1234567890;
+  var answer = 0;
+
+  for (let i = 0; i < a.length; i++) {
+    answer += a[i] * b[i];
+  }
+  console.log(answer);
   return answer;
 }
+
+
+
+const a = [1, 2, 3, 4];
+const b = [-3, -1, 0, 2];
+
+solution(a, b);
